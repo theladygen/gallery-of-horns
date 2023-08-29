9971e0a116eb4e217fbb48b9b4953c2dbaad9911
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2023-08-29 13:59:45 -0700

    created Header, Gallery, Footer files

---
+++ src/Gallery.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Gallery extends React.Component {
+
+  render() {
+    return (
+      <main>
+        <secion>
+          <h2>title</h2>
+          <img src="" alt="" title=""></img>
+          <p>
+            description
+          </p>
+        </secion>
+      </main>
+    );
+  }
+
+}
+
+export default Gallery;
